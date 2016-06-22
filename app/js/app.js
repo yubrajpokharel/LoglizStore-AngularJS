78654301b3382f4005c132ff15eafd27fbc0d1a5
yubraj pokharel <yubraj.pokharel555@gmail.com>
2016-06-22 01:13:09 -0500

    adding directories

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,24 @@
 /**
  * Created by yubraj on 6/22/16.
  */
+
+var app = angular.module('logliz',['ngRoute']);
+
+app.config(function($routeProvider){
+
+    $routeProvider
+        .when('/',{
+            templateUrl: 'views/home.html'
+        })
+        .when('/about',{
+            templateUrl: 'view/about.html',
+            controller:'aboutController'
+        });
+
+
+});
+
+app.controller('aboutController', ['$scope', '$http', function($scope, $http){
+    $http.get("http://www.yubrajpokharel.com/tempstuffs/loglis/about.json")
+        .success(function(response) {$scope.names = response;});
+}]);
